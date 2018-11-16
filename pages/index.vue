5fd69fbf68d563051efbb2816a6c42940486b2e5
weichenyu <weichenyu@ihsmf.com>
2018-11-16 10:03:16 +0800

    nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -6,32 +6,23 @@
         <li><a href="/home">index 页</a></li>
       </ul>
       <div>
-        {{msg}}
+        <p>{{msg.name}}</p>
+        <p>{{msg.age}}</p>
       </div>
-      <ul>
-        <li v-for="(item,index) in info" :key="index">
-          <p>{{item.checkHospital}}</p>
-          <p>{{item.bloodPressure}}</p>
-          <p>{{item.checkTime}}</p>
-          <p>{{item.openId}}</p>
-        </li>
-      </ul>
   </div>
 </template>
 <script>
-  import axios from 'axios'
+import axios from 'axios'
   export default{
     data() {
       return {
-        name: '1232',
+        name: 'name',
         list: []
       }
     },
     async asyncData(){
-        let list = await axios.post('http://47.104.101.207:8080/checkInfo/selectCheckInfo',{"id":125})
         let msg = await axios.get('https://api.myjson.com/bins/1b8ufm')
         return {
-          info: list.data.data,
           msg: msg.data
         }
     }
